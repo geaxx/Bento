1d93caa0e8516530fbc4fcf6cf2e4cd69769c861
geaxx <56831186+geaxx@users.noreply.github.com>
2021-02-10 14:43:44 +0100

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '84f154324df38bb5829de721d7d3e9a9';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 45.489540;
+  let longitude = 9.888130;
 
   getWeather(latitude, longitude);
 }
